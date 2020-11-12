aa4e22b4cf9d15c7c298ab986e8104b0dd42b8fb
Alex Wendland <me@alexwendland.com>
2020-11-11 22:31:52 -0800

    WIP worker_threads + comlink + generic-pool

---
+++ scripts/test_parallel.ts
@@ -0,0 +1,67 @@
+import genericPool from "generic-pool"
+import { Worker, WorkerOptions } from "worker_threads"
+import * as Comlink from "comlink"
+import nodeEndpoint from "comlink/dist/umd/node-adapter"
+
+const workerTs = (file: string, wkOpts: WorkerOptions = {}) => {
+  wkOpts.eval = true
+  if (!wkOpts.workerData) {
+    wkOpts.workerData = {}
+  }
+  wkOpts.workerData.__filename = file
+  return new Worker(
+    `const wk = require('worker_threads');
+     require('ts-node').register();
+     let file = wk.workerData.__filename;
+     delete wk.workerData.__filename;
+     require(file);
+      `,
+    wkOpts
+  )
+}
+
+const generateWorkerFactory = <T>(workerFile: string) =>
+  new (class ComlinkWorkerFactory {
+    #comlinkToWorker = new Map<Comlink.Remote<T>, Worker>()
+
+    async create() {
+      console.log(`parent: create worker`)
+      const worker = workerTs(workerFile)
+      const proxy = Comlink.wrap<T>(nodeEndpoint(worker))
+      this.#comlinkToWorker.set(proxy, worker)
+      return proxy
+    }
+
+    async destroy(proxy: Comlink.Remote<T>) {
+      const worker = this.#comlinkToWorker.get(proxy)
+      worker?.terminate()
+      console.log(`parent: destroy worker-${worker?.threadId}`)
+      proxy[Comlink.releaseProxy]()
+    }
+  })()
+
+const pool = genericPool.createPool(
+  generateWorkerFactory<typeof import("./test_worker")>(
+    __dirname + "/test_worker.ts"
+  ),
+  {
+    min: 1,
+    max: 4, // 1 @ 98%, 2 @ 190%, @ 270%, 8 @ 300%
+  }
+)
+pool.on("factoryCreateError", (e) => console.error(e))
+;(async () => {
+  for (let i = 0; i < 10; i++) {
+    pool
+      .acquire()
+      .then(async (worker) => {
+        console.log(`parent: ${await worker.recursiveFibonacci(45)}`)
+        pool.release(worker)
+      })
+  }
+  await pool.drain()
+  await pool.clear()
+})().catch((e) => {
+  console.error("parent:", e)
+  process.exit(1)
+})
---
+++ scripts/test_worker.ts
@@ -0,0 +1,13 @@
+import { parentPort, threadId } from "worker_threads"
+import * as Comlink from "comlink"
+import nodeEndpoint from "comlink/dist/umd/node-adapter"
+
+console.log(`worker-${threadId}: started`)
+
+export const doHardMath = (howHard: number) =>
+  new Promise((resolve) => setTimeout(() => resolve("good maths"), howHard))
+
+export const recursiveFibonacci = (n: number): number =>
+  n < 2 ? n : recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2)
+
+Comlink.expose(module.exports, nodeEndpoint(parentPort!))
